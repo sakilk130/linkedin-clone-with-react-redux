b01b15e485771c5498cd33847dce737b2f5eec29
sakilk130 <sakilk130@gmail.com>
2021-11-20 22:46:53 +0600

    complete header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,10 @@ import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import { HeaderOption } from './HeaderOption/HeaderOption';
 import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import SmsIcon from '@material-ui/icons/Sms';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function Header() {
   return (
@@ -14,13 +18,16 @@ function Header() {
         />
         <div className="header__search">
           <SearchIcon />
-          <input type="text" />
+          <input type="text" placeholder="Search for jobs, companies.." />
         </div>
       </div>
       <div className="header__right">
         <HeaderOption title={'Home'} Icon={HomeIcon} />
-        <HeaderOption title={'My Network'} />
-        <HeaderOption title={'Home'} />
+        <HeaderOption title={'My Network'} Icon={SupervisorAccountIcon} />
+        <HeaderOption title={'Jobs'} Icon={BusinessCenterIcon} />
+        <HeaderOption title={'Messaging'} Icon={SmsIcon} />
+        <HeaderOption title={'Notifications'} Icon={NotificationsIcon} />
+        <HeaderOption title={'Me'} avatar={'Sakil Khan'} />
       </div>
     </div>
   );
--- src/components/Header/HeaderOption/HeaderOption.tsx
+++ src/components/Header/HeaderOption/HeaderOption.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './HeaderOption.css';
 import PropTypes, { InferProps } from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
 
 export function HeaderOption({
+  avatar,
   Icon,
   title,
 }: InferProps<typeof HeaderOption.propTypes>) {
   return (
     <div className="headerOption">
       {Icon && <Icon className="headerOption__icon" />}
+      {avatar && <Avatar className="headerOption__icon" src={avatar} />}
       <h3 className="headerOption__title">{title}</h3>
     </div>
   );
@@ -17,4 +20,5 @@ export function HeaderOption({
 HeaderOption.propTypes = {
   title: PropTypes.string.isRequired,
   Icon: PropTypes.any,
+  avatar: PropTypes.string,
 };
